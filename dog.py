6d26577005059e2f5ddfd0bb8535e9604c355f11
RogueNPC <jordanbtansay@gmail.com>
2020-11-18 17:59:27 -0800

    instantiated example dog object

--- dog.py
+++ dog.py
@@ -1,4 +1,9 @@
 class Dog:
     def __init__(self, name):
         self.name = name
-        print("dog initialized")
+        print("--dog initialized--")
+
+# calling classes
+my_dog = Dog("Doge")
+print(my_dog)
+print(my_dog.name)
