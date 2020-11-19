9541bea58bb39a501674ebec8a240a631f5b05a9
RogueNPC <jordanbtansay@gmail.com>
2020-11-18 18:04:27 -0800

    added class method

--- dog.py
+++ dog.py
@@ -4,7 +4,11 @@ class Dog:
         self.breed = breed
         print("--dog initialized--")
 
+    def bark(self):
+        print("Woof!")
+
 # calling classes
 my_dog = Dog("Doge", "Shiba Inu")
-print(my_dog)
-print(my_dog.name)
+# print(my_dog.name)
+# print(my_dog.breed)
+my_dog.bark()
