e22a41f10dc7901bd0d31e1d251718d6b6a75a76
RogueNPC <jordanbtansay@gmail.com>
2020-11-21 14:36:12 -0800

    created add_ability and attack method

--- hero.py
+++ hero.py
@@ -18,6 +18,20 @@ class Hero:
         self.abilities = []
         self.armors = []
 
+    def add_ability(self, ability):
+        # We use the append method to add ability objects to our list.
+        self.abilities.append(ability)
+
+    def attack(self):
+        # start our total out at 0
+        total_damage = 0
+        # loop through all of our hero's abilities
+        for ability in self.abilities:
+            # add the damage of each attack to our running total
+            total_damage += ability.attack()
+        # return the total damage
+        return total_damage
+
     def fight(self, opponent):
         # TODO: Fight each hero until a victor emerges.
         # Phases to implement:
@@ -30,8 +44,13 @@ class Hero:
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
     my_hero = Hero("Grace Hopper", 200)
     # print(my_hero.name)
     # print(my_hero.current_health)
-    my_villan = Hero("Firebrand")
-    my_hero.fight(my_villan)
+    my_hero.add_ability(ability)
+    my_hero.add_ability(another_ability)
+    print(my_hero.attack())
+    # my_villan = Hero("Firebrand")
+    # my_hero.fight(my_villan)
