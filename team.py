ad4cd01963402405d0fbc1ec126d34162fc772b0
RogueNPC <jordanbtansay@gmail.com>
2020-11-22 14:44:59 -0800

    created stats, revive_heroes, and attack method in team class

--- team.py
+++ team.py
@@ -1,3 +1,5 @@
+import random
+
 class Team:
     def __init__(self, name):
         self.name = name
@@ -28,4 +30,39 @@ class Team:
         for hero in self.heroes:
             print(hero.name)
 
+    def stats(self):
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print("{} Kill/Deaths:{}".format(hero.name,kd))
+
+    def revive_heroes(self):
+        # TODO: for each hero in self.heroes,
+        # set the hero's current_health to their starting_health
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
     
+    def attack(self, other_team):
+        ''' Battle each team against each other.'''
+
+        living_heroes = []
+        living_opponents = []
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents) > 0:
+            # TODO: Complete the following steps:
+            # 1) Randomly select a living hero from each team (hint: look up what random.choice does)
+            # 2) have the heroes fight each other (Hint: Use the fight method in the Hero class.)
+            # 3) update the list of living_heroes and living_opponents
+            # to reflect the result of the fight
+            hero = random.choice(living_heroes)
+            opponent = random.choice(living_opponents)
+            hero.fight(opponent)
+            if not hero.is_alive():
+                living_heroes.remove(hero)
+            if not opponent.is_alive():
+                living_opponents.remove(opponent)
